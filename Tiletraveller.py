49aa9ebd00cded30621048023765c84156898354
Helga Lind Pétursdóttir <helgalind23@gmail.com>
2022-10-06 13:14:33 +0000

    start

--- Tiletraveller.py
+++ Tiletraveller.py
@@ -1,54 +1,84 @@
+from typing import Tuple
+
+
 # Constants
 NORTH = "n"
 EAST = "e"
 SOUTH = "s"
 WEST = "w"
 
-
-def has_won(col, row):
-    """Return true if player is in the victory cell."""
-
-    return col == 3 and row == 1  # (3,1)
+STARTING_LOCATION = (1, 1)
+FINAL_DESTINATION = (3, 1)
 
 
-def find_directions(col, row):
+def main():
+    location = STARTING_LOCATION
+    while location != FINAL_DESTINATION:
+        location = play_one_move(location)
+
+    print("Victory!")
+
+
+def play_one_move(location: Tuple[int]) -> Tuple[int]:
+    """Plays one move of the game.
+
+    Returns updated location.
+    """
+
+    valid_directions = find_directions(location)
+    direction = get_direction(valid_directions)
+
+    if direction in valid_directions:
+        location = move(direction, location)
+    else:
+        print("Not a valid direction!")
+
+    return location
+
+
+def find_directions(location: Tuple[int]) -> Tuple[str]:
     """Returns valid directions as a string given the supplied location."""
 
-    if col == 1 and row == 1:  # (1,1)
-        valid_directions = NORTH
-    elif col == 1 and row == 2:  # (1,2)
-        valid_directions = NORTH + EAST + SOUTH
-    elif col == 1 and row == 3:  # (1,3)
-        valid_directions = EAST + SOUTH
-    elif col == 2 and row == 1:  # (2,1)
-        valid_directions = NORTH
-    elif col == 2 and row == 2:  # (2,2)
-        valid_directions = SOUTH + WEST
-    elif col == 2 and row == 3:  # (2,3)
-        valid_directions = EAST + WEST
-    elif col == 3 and row == 2:  # (3,2)
-        valid_directions = NORTH + SOUTH
-    elif col == 3 and row == 3:  # (3,3)
-        valid_directions = SOUTH + WEST
+    if location == (1, 1):
+        valid_directions = (NORTH,)
+    elif location == (1, 2):
+        valid_directions = NORTH, EAST, SOUTH
+    elif location == (1, 3):
+        valid_directions = EAST, SOUTH
+    elif location == (2, 1):
+        valid_directions = (NORTH,)
+    elif location == (2, 2):
+        valid_directions = SOUTH, WEST
+    elif location == (2, 3):
+        valid_directions = EAST, WEST
+    elif location == (3, 2):
+        valid_directions = NORTH, SOUTH
+    elif location == (3, 3):
+        valid_directions = SOUTH, WEST
 
     return valid_directions
 
 
-def print_directions(directions_str):
+def get_direction(valid_directions: Tuple[str]) -> str:
+    print_directions(valid_directions)
+    return input("Direction: ").lower()
+
+
+def print_directions(available_directions: Tuple[str]) -> None:
     print("You can travel: ", end="")
 
     one_done_already = False
-    for ch in directions_str:
+    for direction in available_directions:
         if one_done_already:
             print(" or ", end="")
 
-        if ch == NORTH:
+        if direction == NORTH:
             print("(N)orth", end="")
-        elif ch == EAST:
+        elif direction == EAST:
             print("(E)ast", end="")
-        elif ch == SOUTH:
+        elif direction == SOUTH:
             print("(S)outh", end="")
-        elif ch == WEST:
+        elif direction == WEST:
             print("(W)est", end="")
 
         one_done_already = True
@@ -56,44 +86,22 @@ def print_directions(directions_str):
     print(".")
 
 
-def play_one_move(col, row, valid_directions):
-    """Plays one move of the game.
+def move(direction: str, location: Tuple[int]) -> Tuple[int]:
+    """Returns updated location given the direction."""
 
-    Return whether victory has been obtained, and updated col, row.
-    """
-
-    direction = input("Direction: ")
-    direction = direction.lower()
-
-    if direction in valid_directions:
-        col, row = move(direction, col, row)
-    else:
-        print("Not a valid direction!")
-
-    return col, row
-
-
-def move(direction, col, row):
-    """Returns updated col, row given the direction."""
+    x, y = location
 
     if direction == NORTH:
-        row += 1
+        y += 1
     elif direction == SOUTH:
-        row -= 1
+        y -= 1
     elif direction == EAST:
-        col += 1
+        x += 1
     elif direction == WEST:
-        col -= 1
-    return (col, row)
+        x -= 1
+
+    return x, y
 
 
-# The main program starts here
-row = 1
-col = 1
-
-while not has_won(col, row):
-    valid_directions = find_directions(col, row)
-    print_directions(valid_directions)
-    col, row = play_one_move(col, row, valid_directions)
-
-print("Victory!")
+if __name__ == "__main__":
+    main()
